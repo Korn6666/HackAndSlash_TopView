fa50e7c51c38eaddf209af94fbc1b1b46332a302
Korn6666 <simonmanes.sm@gmail.com>
2022-11-29 17:36:53 +0100

    Translation

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(translation,0,0);
+
+    }
+}
