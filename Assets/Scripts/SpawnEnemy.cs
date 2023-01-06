3df9afacf8d4c8c4f6ac8e9e8c5cdef731b92996
margot <margot.pellegrin@telecom-sudparis.eu>
2023-01-06 12:19:49 +0100

    ajout de arene1 et wavespawn

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject EnemyTest;
+    private Vector3 EnemySpawn;
+    void Start()
+    {
+        
+
+    }
+
+    private IEnumerator SpawnanEnemy()
+    {
+        yield return new WaitForSeconds(0f);
+        Instantiate(EnemyTest, EnemySpawn, Quaternion.identity);
+    }
+
+    public void StartSpawnEnemy()
+    {
+        GameObject[] spawnpoints = GameObject.FindGameObjectsWithTag("Spawn");
+        int index = Random.Range(0, spawnpoints.Length);
+        GameObject currentPoint = spawnpoints[index];
+        if (!currentPoint) return;
+        EnemySpawn = currentPoint.transform.position;
+        StartCoroutine(SpawnanEnemy());
+    }
+}
